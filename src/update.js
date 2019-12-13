6a3ee1d626667cd6dfc08777c4310b566ef1d1b3
Vecheka Chhourn <vecheka.chhourn@nordstrom.com>
2019-12-13 13:39:03 -0800

    added update function

---
+++ src/update.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Schema = require('./common/schema.js');
+const db = require('./common/db.js');
+
+const { Client } = require('pg');
+const client = new Client(db);
+client.connect();
+
+exports.handler = async(event, context) => {
+// async function handler() {
+  const table = event.pathParameters.table;
+  // const table = event.pathParameters.id;
+  const body = JSON.parse(event.body);
+  // const table = "access_item";
+  // const body = {"subnet": "10.1.1.1", "id": 2, 'description': 'testing'};
+  const schema = await Schema.build(client, table, 'update');
+  const valid = Schema.validate(body, schema, 'update');
+  console.log(valid);
+  if (!valid.valid)
+    return {
+      "statusCode": 400,
+      "body": JSON.stringify(valid.errs)
+    }
+
+  const query = buildQuery(table, schema, body);
+  // console.log(query);
+  let res = null;
+  try {
+    res = await client.query(query.query, query.values);
+    console.log(res);
+  } catch (err) {
+    // console.log(err);
+    return {
+      "statusCode": 500,
+      "body": JSON.stringify(err)
+    }
+  }
+
+  return {
+    "statusCode": 200,
+    "body": JSON.stringify({id: res.rows[0].id})
+  }
+}
+
+function buildQuery(table, schema, body) {
+  let query = `UPDATE ${table}`;
+  let cols = ' SET ';
+  let vals = 'VALUES(';
+  let valsArr = [];
+  let i = 1;
+  let id;
+  for (let key in schema.properties) {
+    if (typeof body[key] !== 'undefined') {
+      if (key === 'id') {
+          id = body[key];
+          continue;
+      }
+      if (i > 1) {
+        cols += `, ${key} = $${i}`;
+      } else {
+        cols += `${key} = $${i}`;
+      }
+
+      valsArr.push(body[key]);
+      i++;
+    }
+  }
+  valsArr.push(id);
+  return {
+    query: `${query}${cols} WHERE id = $${i} RETURNING id`,
+    values: valsArr
+  }
+
+}
+// handler();
