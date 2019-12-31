07173e934baf5cf2a1a1a7d195382373772e93d0
Vecheka Chhourn <vecheka.chhourn@nordstrom.com>
2019-12-31 11:20:53 -0800

    add env file

---
+++ src/common/db.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const db = {
+    host: process.env.NSG_DB_URL, // server name or IP address;
+    port: process.env.NSG_DB_PORT,
+    database: process.env.NSG_DB_NAME,
+    user: process.env.NSG_DB_USER,
+    password: process.env.NSG_DB_PSSW,
+    query_timeout: 5500
+};
+
+module.exports = db;
