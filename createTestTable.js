e3f0712ad7025e6160c882659cec3ef915b8bdf3
Vecheka Chhourn <vecheka.chhourn@nordstrom.com>
2020-01-02 14:55:56 -0800

    testing external db

--- createTestTable.js
+++ createTestTable.js
@@ -1,4 +1,5 @@
 const db = require('./src/common/db.js');
+console.log(db);
 const { Client } = require('pg');
 const client = new Client(db);
 
--- src/common/db.js
+++ src/common/db.js
@@ -1,12 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const db = {
-    host: process.env.NSG_DB_URL, // server name or IP address;
+    // host: process.env.NSG_DB_URL, // server name or IP address;
+    host: 'nsgexternaldb.clqzasuuu8wm.us-west-2.rds.amazonaws.com',
     port: 5432,
-    database: process.env.NSG_DB_NAME,
+    // database: process.env.NSG_DB_NAME,
+    database: 'nsgExternalDB',
     user: process.env.NSG_DB_USER,
     password: process.env.NSG_DB_PSSW,
-    query_timeout: 5500
+    // query_timeout: 5500
 };
 
 module.exports = db;
