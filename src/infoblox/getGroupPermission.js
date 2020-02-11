c61056567bbb8e7aeb46e496d03f383f87344e28
Vecheka Chhourn <vecheka.chhourn@nordstrom.com>
2020-02-11 08:17:30 -0800

    infoblox api endpoint to get group permission

--- src/infoblox/getGroupPermission.js
+++ src/infoblox/getGroupPermission.js
@@ -3,10 +3,10 @@
 const corsHeaders = require('../common/headers.js');
 const responder = require('../common/respond.js');
 const auth = require('../common/auth.js');
-const fetch = require("node-fetch").default;
+const fetch = require('node-fetch').default;
 const https = require('https');
 const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
+  rejectUnauthorized: false
 });
 const domain = 'https://infoblox.nordstrom.net/wapi/v2.10.3';
 
@@ -15,8 +15,8 @@ exports.handler = async (event, context, callback, test = false) => {
   // const headers = {
   //   'Access-Control-Allow-Origin': 'something'
   // };
-  const token = await auth.verifyToken(event.headers.Authorization);
 
+  const token = await auth.verifyToken(event.headers.Authorization);
   const logPayload = {};
   if (!token.valid && test === false) {
     logPayload.error = token.err.name + ' ' + token.err.userMessage;
@@ -24,25 +24,27 @@ exports.handler = async (event, context, callback, test = false) => {
   } else
     logPayload.user = token.jwt.claims.sub;
 
-  let response = await getGroups('nsg', process.env.NSG_DB_PASS);
+  let response = await getGroups('nsg', 'nsgseattle');
+  if ('error' in response) return responder.respond(500, response, headers, logPayload);
   let statusCode = response.status;
   if (statusCode !== 200) {
     logPayload.err = response.statusText;
-    return responder.respond(statusCode, { 'msg': response.statusText }, headers, logPayload);
+    return responder.respond(statusCode, { msg: response.statusText }, headers, logPayload);
   }
   const groups = await response.json();
 
-  response = await getPermissions('nsg', process.env.NSG_DB_PASS);
+  response = await getPermissions('nsg', 'nsgseattle');
+  if ('error' in response) return responder.respond(500, response, headers, logPayload);
   statusCode = response.status;
   if (statusCode !== 200) {
     logPayload.err = response.statusText;
-    return responder.respond(statusCode, { 'msg': response.statusText }, headers, logPayload);
+    return responder.respond(statusCode, { msg: response.statusText }, headers, logPayload);
   }
   const permissions = await response.json();
   const results = {
     groups: groups,
     permissions: permissions
-  }
+  };
   return responder.respond(200, results, headers, logPayload);
 };
 
@@ -52,28 +54,27 @@ async function getGroups (username, password) {
     const fields = '_return_fields=roles,name';
     const opts = {
       method: 'GET',
-      headers: { Authorization: 'Basic ' + new Buffer(username + ':' + password).toString('base64') },
+      headers: { Authorization: 'Basic ' + Buffer.from(username + ':' + password).toString('base64') },
       agent: httpsAgent
     };
     response = await fetch(`${domain}/admingroup?${fields}`, opts);
     return response;
   } catch (err) {
-    console.log(err);
-    return null;
+    return { error: err };
   }
 }
 
 async function getPermissions (username, password) {
-    let response;
-    try {
-      const opts = {
-        method: 'GET',
-        headers: { Authorization: 'Basic ' + new Buffer(username + ':' + password).toString('base64') },
-        agent: httpsAgent
-      };
-      response = await fetch(`${domain}/permission`, opts);
-      return response;
-    } catch (err) {
-      return null;
-    }
+  let response;
+  try {
+    const opts = {
+      method: 'GET',
+      headers: { Authorization: 'Basic ' + Buffer.from(username + ':' + password).toString('base64') },
+      agent: httpsAgent
+    };
+    response = await fetch(`${domain}/permission`, opts);
+    return response;
+  } catch (err) {
+    return { error: err };
   }
+}
