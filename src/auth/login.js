f26eb5f76f47fd06fe44db449c82aada36693f1c
Vecheka Chhourn <vecheka.chhourn@nordstrom.com>
2020-02-20 14:39:21 -0800

    structured the file

--- src/auth/login.js
+++ src/auth/login.js
@@ -2,14 +2,14 @@
 
 const corsHeaders = require('../common/headers.js');
 const { respond } = require('../common/respond.js');
-const user = require('../common/user.js');
+const authorizer = require('../common/auth.js');
 
 exports.handler = async (event, context, callback) => {
   const headers = corsHeaders.verifyOrigin(event.headers.origin);
   const creds = event.headers.Authorization;
 
-  const auth = await user.verifyUser(creds);
-  console.log(auth);
+  const auth = await authorizer.verifyToken(creds);
+
   const logPayload = {};
   if (!auth.valid) {
     logPayload.error = auth.err;
--- src/common/auth.js
+++ src/common/auth.js
@@ -2,7 +2,9 @@ const Okta = require('@okta/jwt-verifier');
 const verifier = new Okta({
   issuer: 'https://nordstrom.oktapreview.com/oauth2/ausg0zujx3faqpGaA0h7'
 });
-const user = require('./user.js');
+
+const db = require('./db.js');
+const { Client } = require('pg');
 
 module.exports.verifyToken = async (token) => {
   if (!token)
@@ -19,12 +21,60 @@ module.exports.verifyToken = async (token) => {
         jwt: jwt
       };
     } catch (err) {
-      console.log(err);
       return {
         valid: false,
         err: err
       };
     }
   } else if (token.startsWith('Basic '))
-    return user.verifyUser(token);
+    return verifyUser(token);
+};
+
+async function verifyUser (creds) {
+  if (!creds)
+    return { valid: false, err: 'No auth', status: 403 };
+
+  let cursor;
+  if (creds.startsWith('Basic '))
+    creds = creds.substr(5);
+
+  creds = Buffer.from(creds, 'base64').toString();
+
+  const username = creds.split(':')[0];
+  const password = creds.split(':')[1];
+
+  const client = new Client(db);
+  try {
+    await client.connect();
+  } catch (err) {
+    return {
+      valid: false,
+      status: 500,
+      err: 'database connection error'
+    };
+  }
+
+  const query = 'SELECT id FROM orca.user WHERE username=$1 and password=$2';
+  const values = [username, password];
+  try {
+    cursor = await client.query(query, values);
+  } catch (err) {
+    return {
+      valid: false,
+      status: 400,
+      err: 'query error'
+    };
+  }
+  if (cursor.rows.length >= 1) {
+    return {
+      valid: true,
+      status: 200,
+      username: username
+    };
+  }
+  return {
+    valid: false,
+    status: 403,
+    err: 'Forbidden'
+  };
 };
